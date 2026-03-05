c1c6eb45d1f86d39828ecd241620a9cf3e7817e6
nickkossolapov <nickkossolapov@gmail.com>
2026-03-05 17:26:21 +0000

    Add Colemak keymapping

--- src/FameBoy.Raylib/Joypad.fs
+++ src/FameBoy.Raylib/Joypad.fs
@@ -4,15 +4,27 @@ open FameBoy.Joypad
 open FameBoy.Raylib.RaylibBindings
 open Raylib_cs
 
+// QWERTY
+// let joypadMapping =
+//     {| Up = KeyboardKey.W
+//        Down = KeyboardKey.S
+//        Left = KeyboardKey.A
+//        Right = KeyboardKey.D
+//        A = KeyboardKey.J
+//        B = KeyboardKey.K
+//        Start = KeyboardKey.N
+//        Select = KeyboardKey.M |}
+
+// Colemak-dh
 let joypadMapping =
-    {| Up = KeyboardKey.W
+    {| Up = KeyboardKey.F
        Down = KeyboardKey.S
-       Left = KeyboardKey.A
-       Right = KeyboardKey.D
-       A = KeyboardKey.J
-       B = KeyboardKey.K
-       Start = KeyboardKey.N
-       Select = KeyboardKey.M |}
+       Left = KeyboardKey.R
+       Right = KeyboardKey.T
+       A = KeyboardKey.N
+       B = KeyboardKey.E
+       Start = KeyboardKey.H
+       Select = KeyboardKey.K |}
 
 let getJoypadState () : JoypadState =
     { Up = isKeyDown joypadMapping.Up
