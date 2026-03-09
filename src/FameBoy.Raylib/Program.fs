296da93717e3dc325c3fa60e0227e15b978a67f6
nickkossolapov <nickkossolapov@gmail.com>
2026-03-09 21:25:46 +0000

    Extract main loop to be able to use different renderers

--- src/FameBoy.Raylib/Program.fs
+++ src/FameBoy.Raylib/Program.fs
@@ -1,15 +1,11 @@
 ﻿open System.IO
-open FameBoy.Cpu.Execute
-open FameBoy.Graphics.Ppu
+open FameBoy.Emulator
 open FameBoy.Joypad
-open FameBoy.Memory
 open FameBoy.Raylib
 open FameBoy.Raylib.Graphics.GraphicsPipeline
 open FameBoy.Raylib.Joypad
 open FameBoy.Raylib.RaylibBindings
 open FameBoy.Raylib.Utils.RateLimiting
-open FameBoy.Startup
-open FameBoy.Timer
 open Raylib_cs
 
 Raylib.InitWindow(Config.width * Config.scale, Config.height * Config.scale, "Fame Boy")
@@ -17,19 +13,10 @@ Raylib.SetTargetFPS 60
 
 let mcyclesPerSec = 1000 / 60
 
-let printLastFrameTime =
-    rateLimitFunc 1000 (fun () -> printfn $"{1f / Raylib.GetFrameTime()}")
-
 let printBits =
     rateLimitFunc 1000 (fun (s: uint8) -> printfn $"{System.Convert.ToString(s, 2).PadLeft(8, '0')}")
 
-// let bytes = File.ReadAllBytes "D:/gb/tetris.gb"
-// let bytes = File.ReadAllBytes "/Users/nickkossolapov/dev/gb/tetris.gb"
-// let bytes = File.ReadAllBytes "/Users/nickkossolapov/dev/gb/dr mario.gb"
-// let bytes = File.ReadAllBytes "D:/gb/test-roms/cpu_instrs/cpu_instrs.gb"
-let bytes = File.ReadAllBytes "D:/gb/tetris.gb"
-
-let frameTimesDuration = 600 // frame
+let frameTimesDuration = 120 // frame
 let frameTimes = Array.create frameTimesDuration 60f
 let mutable frameIndex = 0
 
@@ -38,10 +25,19 @@ let printTotalFrameTime =
         let avg = Array.average frameTimes
         printfn $"avg: %.4f{1f / avg} | last frame: %.4f{1f / Raylib.GetFrameTime()}")
 
-let timer = createTimer ()
-let memory = createMemory bytes
-let cpu = createDmgCpu memory
-let ppu = createPpu memory
+let bytes = File.ReadAllBytes "D:/gb/tetris.gb"
+
+let mutable joypadState: JoypadState =
+    { Up = false
+      Down = false
+      Left = false
+      Right = false
+      A = false
+      B = false
+      Start = false
+      Select = false }
+
+let struct (frameBuffer, memory, stepEmulator) = createEmulator bytes (fun () -> joypadState)
 
 while (not (windowShouldClose ())) do
     // TODO: have a better frame time counter
@@ -49,26 +45,16 @@ while (not (windowShouldClose ())) do
     frameTimes[frameIndex] <- Raylib.GetFrameTime()
     frameIndex <- (frameIndex + 1) % frameTimesDuration
 
-    let joypadState = getJoypadState ()
+    joypadState <- getJoypadState ()
 
     printTotalFrameTime ()
 
     while (counter > 0) do
-        // TODO don't apply on every instruction. Modify memory to resolve joypad state on read, and handle interrupts
-        applyJoypadState joypadState memory
-        let cpuCycles = stepCpu cpu
+        let cpuCycles = stepEmulator ()
         counter <- counter - cpuCycles
 
-        for _ in 1..cpuCycles do
-            stepTimers timer memory
-
-        let ppuSteps = cpuCycles * 4
-
-        for _ in 0..ppuSteps do
-            stepPpu ppu
-
     beginDrawing ()
-    loadPpuFramebuffer ppu.Framebuffer
+    loadPpuFramebuffer frameBuffer
 
     if Config.enableDebugView then
         loadDebugFramebuffers memory
---
+++ src/FameBoy/Emulator.fs
@@ -0,0 +1,32 @@
+﻿module FameBoy.Emulator
+
+open FameBoy.Cpu.Execute
+open FameBoy.Graphics.Ppu
+open FameBoy.Joypad
+open FameBoy.Memory
+open FameBoy.Startup
+open FameBoy.Timer
+
+
+let createEmulator bytes getJoypadState =
+    let timer = createTimer ()
+    let memory = createMemory bytes
+    let cpu = createDmgCpu memory
+    let ppu = createPpu memory
+
+    let stepper () =
+        // TODO don't apply on every instruction. Modify memory to resolve joypad state on read, and handle interrupts
+        applyJoypadState (getJoypadState ()) memory
+        let cpuCycles = stepCpu cpu
+
+        for _ in 1..cpuCycles do
+            stepTimers timer memory
+
+        let ppuSteps = cpuCycles * 4
+
+        for _ in 1..ppuSteps do
+            stepPpu ppu
+
+        cpuCycles
+
+    struct (ppu.Framebuffer, memory, stepper)
