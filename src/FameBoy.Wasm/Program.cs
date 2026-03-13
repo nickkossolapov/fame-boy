4b55d275244b582a2d4111d3fb20e5d94cc549a6
nickkossolapov <nickkossolapov@gmail.com>
2026-03-13 17:56:27 +0000

    Attempted to use web assembly instead, but perf is not great :(

--- src/FameBoy.Wasm/Program.cs
+++ src/FameBoy.Wasm/Program.cs
@@ -1,57 +1,68 @@
 using System;
-using System.Diagnostics;
 using System.Runtime.InteropServices.JavaScript;
-using System.Threading.Tasks;
-
-
+using FameBoy;
+using FameBoy.Graphics;
+using Microsoft.FSharp.Core;
 
 Console.WriteLine("Hello, Browser!");
 
-if (args.Length == 1 && args[0] == "start")
-    StopwatchSample.Start();
-
-while (true)
+internal class EmulatorContainer(Ppu.Shade[] framebuffer, Func<int> step)
 {
-    StopwatchSample.Render();
-    await Task.Delay(1000);
+    private readonly int[] _outputFramebuffer = new int[framebuffer.Length];
+
+    public void Step()
+    {
+        step();
+    }
+
+    public int[] GetOutputFramebuffer()
+    {
+        for (var i = 0; i < framebuffer.Length; i++)
+        {
+            var (r, g, b) = framebuffer[i] switch
+            {
+                Ppu.Shade.White => (186, 218, 85),
+                Ppu.Shade.Light => (130, 153, 59),
+                Ppu.Shade.Dark => (74, 87, 34),
+                Ppu.Shade.Black => (19, 22, 8),
+                _ => (0, 0, 0)
+            };
+
+            const int a = 255 << 24;
+
+            _outputFramebuffer[i] =
+                a |
+                (b << 16) |
+                (g << 8) |
+                r;
+        }
+
+        return _outputFramebuffer;
+    }
 }
 
-partial class StopwatchSample
+internal partial class EmulatorInterop
 {
-    private static Stopwatch stopwatch = new();
-
-    public static void Start() => stopwatch.Start();
-    public static void Render() => SetInnerText("#time", stopwatch.Elapsed.ToString(@"mm\:ss"));
-    
-    [JSImport("dom.setInnerText", "main.js")]
-    internal static partial void SetInnerText(string selector, string content);
+    private static readonly Joypad.JoypadState _joypad = new(false, false, false, false, false, false, false, false);
+    private static EmulatorContainer? _emulator;
 
     [JSExport]
-    internal static bool Toggle()
+    internal static void Init(byte[] bytes)
     {
-        if (stopwatch.IsRunning)
-        {
-            stopwatch.Stop();
-            return false;
-        }
-        else
-        {
-            stopwatch.Start();
-            return true;
-        }
+        var (framebuffer, _, step) = Emulator.createEmulator(bytes, FuncConvert.FromFunc(() => _joypad));
+
+        _emulator = new EmulatorContainer(framebuffer, () => step.Invoke(null));
+        Console.Write($"Read {bytes.Length} bytes");
     }
 
     [JSExport]
-    internal static void Reset()
+    internal static int[] Step(int steps)
     {
-        if (stopwatch.IsRunning)
-            stopwatch.Restart();
-        else
-            stopwatch.Reset();
+        for (var i = 0; i < steps; i++) _emulator?.Step();
 
-        Render();
+        return _emulator?.GetOutputFramebuffer() ?? [];
     }
 
-    [JSExport]
-    internal static bool IsRunning() => stopwatch.IsRunning;
+    // [JSImport("emulator.getJoypadState", "main.js")]
+    // internal static partial byte GetJoypadState(); // bool[] is not supported by .NET-JS interop
 }
